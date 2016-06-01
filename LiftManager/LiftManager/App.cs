0551240b4298d2f867742d714c8ec75a1e500a96
Stewart Trezise <stewart.trezise@gmail.com>
2016-06-01 18:46:18 +1000

    Added a simple (style-driven) splash screen for Android.

--- LiftManager/LiftManager/App.cs
+++ LiftManager/LiftManager/App.cs
@@ -13,20 +13,6 @@ namespace LiftManager
         public App()
         {
             // The root page of your application
-            /*
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };*/
             MainPage = new LiftSimulationScreen();
         }
 
